707a1e8867482fcd7a6618f0bb66d4395a95cb8b
Stéphane Albert <sheeprine@oh.its.fake.nullplace.com>
2021-01-16 09:33:09 +0100

    Add support for Arch linux
    Removed the use of cron for nvim cleanup on Arch and uses systemd
    timers.

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,5 +10,7 @@ else
     sudo yum update && sudo yum install -y ansible
   elif [ $(which apt) ]; then
     sudo apt update && sudo apt install -y ansible
+  elif [ $(which pacman) ]; then
+    sudo pacman -Sy && sudo pacman -S --noconfirm --needed ansible
   fi
 fi
